87d1b0da43fdc89dc06fbbf83c41b89b397a5beb
Alexandr G <alexandr@gavrisco.com>
2015-11-16 20:38:59 +0200

    Add redirection to login page for private pages if user isn't authenticated.

--- app/custom/authentication/auth.module.js
+++ app/custom/authentication/auth.module.js
@@ -3,5 +3,14 @@
 
     angular.module('bakimliAuth', [
         'ui.router'
-        ]);
+        ]).run(['$rootScope', '$state', 'AuthFactory', runFn]);
+
+    function runFn($rootScope, $state, authFactory) {
+        $rootScope.$on('$stateChangeStart', function(event, stateTo, paramsTo, stateFrom, paramsFrom) {
+            if (stateTo.data.isPrivate && !authFactory.isAuthenticated()) {
+                event.preventDefault();
+                $state.go('login');
+            }
+        });
+    }
 })();
