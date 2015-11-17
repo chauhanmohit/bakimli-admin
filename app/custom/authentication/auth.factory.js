94e97bc91bc437f52f9a7b9cf336b4c8c685e342
Alexandr G <alexandr@gavrisco.com>
2015-11-17 11:04:05 +0200

    Add login method to the AuthFactory.

--- app/custom/authentication/auth.factory.js
+++ app/custom/authentication/auth.factory.js
@@ -2,13 +2,51 @@
     'use strict';
 
     angular.module('bakimliAuth').factory('AuthFactory', [
+        '$localStorage',
+        '$rootScope',
+        '$q',
+        '$http',
+        'APIFormat',
         authFactory
         ]);
 
-    function authFactory() {
+    function authFactory($localStorage, $rootScope, $q, $http, apiFormat) {
+        var checkUser = function() {
+            if (typeof $localStorage.user !== 'object') {
+                return false;
+            }
+            if ($rootScope !== $localStorage.user) {
+                $rootScope.user = angular.copy($localStorage.user);
+            }
+            return true;
+        };
+
         return {
             isAuthenticated: function() {
-                return false;
+                return checkUser();
+            },
+            login: function(credentials) {
+                var deferred = $q.defer();
+                $http.post(apiFormat.fmtV1url('/api/v1/auth/login/'), credentials).then(function(response) {
+                    if (response.status !== 200) {
+                        deferred.reject(response);
+                        return;
+                    }
+                    if (typeof response.data !== 'object') {
+                        deferred.reject(response);
+                        return;
+                    }
+                    if (typeof response.data.token === 'undefined') {
+                        deferred.reject(response);
+                        return;
+                    }
+                    $localStorage.user = angular.copy(response.data);
+                    checkUser();
+                    deferred.resolve($localStorage.user);
+                }, function(response) {
+                    deferred.reject(response);
+                });
+                return deferred.promise;
             }
         };
     }
