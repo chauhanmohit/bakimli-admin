480b0f54db9fe6b72fe486b0d7f3aac48f2801a6
Alexandr G <alexandr@gavrisco.com>
2015-11-17 11:03:17 +0200

    Add utils module. Add formatting of url to API, because API is on other address.

---
+++ app/custom/utils/api-format.factory.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    angular.module('bakimliUtils').factory('APIFormat', [function() {
+        var protocol = 'http',
+            port = '8001',
+            hostname = 'localhost';
+
+        return {
+            fmtV1url: function (url) {
+                if (url.indexOf('/') !== 0) {
+                    url = '/' + url;
+                }
+                return protocol + '://' + hostname + ':' + port + url;
+            }
+        };
+    }]);
+})();
