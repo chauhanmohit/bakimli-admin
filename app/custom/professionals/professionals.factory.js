9dec3fd41029eabb7193ead35dce1c3354f3e506
Alexandr G <alexandr@gavrisco.com>
2015-11-19 17:29:20 +0200

    Add factory to create/retrieve/update professionals.

---
+++ app/custom/professionals/professionals.factory.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    angular.module('bakimliProfessionals').factory('Professionals', [
+        '$q', '$http', 'APIFormat', professionalsFactory
+        ]);
+
+    function professionalsFactory($q, $http, apiFormat) {
+        var createNewProfessional = function (data) {
+                var deferred = $q.defer();
+                var options = null;
+                if (data instanceof FormData) {
+                    options = {
+                        transformRequest: angular.identity,
+                        headers: {'Content-Type': undefined}
+                    };
+                }
+                $http.post(apiFormat.fmtV1url('/professionals/'), data, options).then(function(response) {
+                    if (response.status === 201) {
+                        deferred.reject(response);
+                    }
+                    deferred.resolve(response.data);
+                }, function(response) {
+                    deferred.reject(response);
+                });
+                return deferred.promise;
+            },
+            getProfessional = function (professionalId) {
+                var deferred = $q.defer();
+                $http.get(apiFormat.fmtV1url('/professionals/' + professionalId + '/')).then(function (response) {
+                    deferred.resolve(response.data);
+                }, function (response) {
+                    deferred.reject(response);
+                });
+                return deferred.promise;
+            };
+
+        return {
+            create: createNewProfessional,
+            get: getProfessional
+        };
+    }
+})();
