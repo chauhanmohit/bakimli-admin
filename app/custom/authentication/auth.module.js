85d452b5a53c9b8a87c870655ebddd7cd26d4c42
Alexandr G <alexandr@gavrisco.com>
2015-11-19 13:10:21 +0200

    Add checks for authorization.

--- app/custom/authentication/auth.module.js
+++ app/custom/authentication/auth.module.js
@@ -5,13 +5,31 @@
         'ui.router',
         'ngStorage',
         'bakimliUtils'
-        ]).run(['$rootScope', '$state', 'AuthFactory', runFn]);
+        ]).run(['$rootScope', '$state', 'AuthFactory', 'AuthorizationFactory', runFn]);
 
-    function runFn($rootScope, $state, authFactory) {
+    function runFn($rootScope, $state, authFactory, authorization) {
         $rootScope.$on('$stateChangeStart', function(event, stateTo, paramsTo, stateFrom, paramsFrom) {
+            if (!stateTo.data) {
+                return;
+            }
             if (stateTo.data.isPrivate && !authFactory.isAuthenticated()) {
                 event.preventDefault();
                 $state.go('login');
+                return;
+            }
+            var permissionMatch = authorization.hasPermissionForState(stateTo, paramsTo);
+            if (permissionMatch && !permissionMatch.status) {
+                event.preventDefault();
+                if (typeof stateTo.data.redirectTo === 'string') {
+                    $state.go(stateTo.data.redirectTo);
+                    return;
+                }
+                if (typeof stateTo.data.redirectTo === 'object' && stateTo.data.redirectTo.hasOwnProperty(permissionMatch.permission)) {
+                    $state.go(stateTo.data.redirectTo[permissionMatch.permission]);
+                    return;
+                }
+                $state.go('login');
+                return;
             }
         });
     }
