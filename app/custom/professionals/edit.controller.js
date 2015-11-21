2d420c17ce8b6790190527d86d2a52eb52e7442b
Alexandr G <alexandr@gavrisco.com>
2015-11-21 14:52:31 +0200

    Update edit form. Add address, salon and finish configuration of submit.

--- app/custom/professionals/edit.controller.js
+++ app/custom/professionals/edit.controller.js
@@ -2,11 +2,58 @@
     'use strict';
 
     angular.module('bakimliProfessionals').controller('ProfessionalUpdateController', [
-        'Professionals', 'FormUtils', professionalCtrl
+        'Professionals', 'FormUtils', 'Salons', 'Districts', professionalCtrl
         ]);
 
-    function professionalCtrl(professionals, formUtils) {
+    function professionalCtrl(professionals, formUtils, salons, districts) {
         var self = this;
+
+        salons.query(null, function(result) {
+            self.salons.data = result.results;
+        });
+
+        districts.query(null, function(result) {
+            self.districts.data = result.results;
+        });
+
+        self.employmentType = {
+            option: [
+            {id: 1, value: 'free', title: 'I am a freelancer'},
+            {id: 2, value: 'salon', title: 'I work at a salon'}
+            ],
+            config: {
+                create: false,
+                maxItems: 1,
+                placeholder: 'Select...',
+                valueField: 'value',
+                labelField: 'title',
+                searchField: 'title'
+            }
+        };
+
+        self.salons = {
+            data: [],
+            config: {
+                create: false,
+                maxItems: 1,
+                placeholder: 'Select...',
+                valueField: 'pk',
+                labelField: 'name',
+                searchField: 'name'
+            }
+        };
+
+        self.districts = {
+            data: [],
+            config: {
+                create: false,
+                maxItems: 1,
+                placeholder: 'District',
+                valueField: 'pk',
+                labelField: 'full_name',
+                searchField: 'full_name'
+            }
+        };
 
         self.submit = function (form) {
             if (!form.$valid) {
