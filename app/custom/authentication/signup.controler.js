089055352446d748b53027e8f9e0932052188854
Alexandr G <alexandr@gavrisco.com>
2015-11-23 16:28:53 +0200

    Add dummy models.

--- app/custom/authentication/signup.controler.js
+++ app/custom/authentication/signup.controler.js
@@ -8,6 +8,10 @@
     function signupCtrl() {
         var self = this;
 
+        self.user = {}; // user model
+        self.professional = {}; // base professional fields
+        self.contactInfo = {}; // contact fields
+
         self.isUserValid = function () {
             var form = $("#wizard_advanced_form").parsley();
             form.validate();
