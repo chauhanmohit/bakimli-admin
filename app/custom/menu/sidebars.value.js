4a846614ba846f7885db796c770bfeeaf6dffaa7
Alexandr G <alexandr@gavrisco.com>
2015-11-23 12:07:36 +0200

    Add menu module (now to hold sidebars).

---
+++ app/custom/menu/sidebars.value.js
@@ -0,0 +1,62 @@
+(function() {
+    'use strict';
+
+    angular.module('bakimliMenu').value('Sidebars', {
+        anonymousUser: [
+            {
+                title: 'Login',
+                icon: '&#xE7FF;',
+                link: 'login'
+            },
+            {
+                title: 'Sign Up',
+                icon: '&#xE7FE;',
+                link: 'signup'
+            },
+            {
+                title: 'Blog',
+                icon: '&#xE0BF;',
+                link: 'app.blog.index'
+            },
+            {
+                title: 'Help Center',
+                icon: '&#xE887;',
+                link: 'app.help'
+            }
+        ],
+        activeUser: [
+            {
+                title: 'Dashboard',
+                icon: '&#xE871;',
+                link: 'app.dashboard'
+            },
+            {
+                title: 'Blog',
+                icon: '&#xE0BF;',
+                link: 'app.blog.index'
+            },
+            {
+                title: 'Help Center',
+                icon: '&#xE887;',
+                link: 'app.help'
+            }
+        ],
+        inactiveUser: [
+            {
+                title: 'Professional Profile',
+                icon: '&#xE851;',
+                link: 'app.professional.wizard'
+            },
+            {
+                title: 'Blog',
+                icon: '&#xE0BF;',
+                link: 'app.blog.index'
+            },
+            {
+                title: 'Help Center',
+                icon: '&#xE887;',
+                link: 'app.help'
+            }
+        ]
+    });
+})();
