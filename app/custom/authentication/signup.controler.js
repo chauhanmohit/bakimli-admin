9a8133d88b3f1699e531c83c6f357c0660d8c87f
Alexandr G <alexandr@gavrisco.com>
2015-11-24 12:59:46 +0200

    Integrate API calls with vizard.

--- app/custom/authentication/signup.controler.js
+++ app/custom/authentication/signup.controler.js
@@ -2,11 +2,25 @@
     'use strict';
 
     angular.module('bakimliAuth').controller('SignupController', [
-        '$q', 'APIFormat', 'Salons', 'Districts', signupCtrl
+        '$log', '$timeout', '$q', '$state', '$stateParams', 'WizardHandler',
+        'AuthFactory', 'AuthorizationFactory', 'APIFormat',
+        'Salons', 'Districts', 'Professionals', signupCtrl
         ]);
 
-    function signupCtrl($q, apiFormat, salons, districts) {
-        var self = this;
+    function signupCtrl(
+            $log, $timeout, $q, $state, $stateParams, WizardHandler,
+            authFactory, authorization, apiFormat,
+            salons, districts, professionals) {
+        var self = this, userCreated = false, professionalCreated = false;
+        $timeout(function () {
+            if (authFactory.isAuthenticated() && !authorization.hasProfile()) {
+                userCreated = true;
+                WizardHandler.wizard().goTo(1);
+            } else if (authorization.hasProfile()) {
+                userCreated = professionalCreated = true;
+                WizardHandler.wizard().goTo(3);
+            }
+        }, 500);
 
         self.emailValidationURL = apiFormat.fmtV1url('/auth/check-email/');
         self.user = {}; // user model
@@ -49,6 +63,9 @@
         });
 
         self.isUserValid = function () {
+            if (userCreated) {
+                return true;
+            }
             var deferred = $q.defer();
             var form = $("#wizard_advanced_form").parsley();
             form.whenValidate().then(function () {
@@ -71,5 +88,65 @@
                 self.contactInfo.postal_code = null;
             }
         };
+
+        self.finishSignup = function () {
+            if (userCreated && professionalCreated) {
+                return;
+            }
+            var professionalPromise;
+            if (!userCreated) {
+                professionalPromise = authFactory.signup(self.user).then(function () {
+                    userCreated = true;
+                    var professionalModel = prepareProfessionalModel(self.user.name, self.professional, self.contactInfo);
+                    return professionals.create(professionalModel);
+                }, function (response) {
+                    // TODO: add user-friendly output
+                    userCreated = false;
+                    $log.debug('Signup error ', response);
+                });
+            } else {
+                var professionalModel = prepareProfessionalModel(self.user.name, self.professional, self.contactInfo);
+                professionalPromise = professionals.create(professionalModel);
+            }
+            return professionalPromise.then(function () {
+                professionalCreated = true;
+                $state.go('app.dashboard');
+            }, function (response) {
+                // TODO: add user-friendly validation
+                if (response.status === 400) {
+                    UIkit.notify({
+                        message: 'Please check your basic and contact info. Provide a valid phone number and address.',
+                        status: 'warning',
+                        timeout: 5000
+                    });
+                }
+                $log.debug('Error while saving professional ', response.status, response.data);
+                professionalCreated = false;
+            });
+        };
+
+        self.canEnterUserInfo = function () {
+            return !userCreated;
+        };
+        self.canEnterBasicInfo = function () {
+            return !professionalCreated;
+        };
+        self.canEnterContactInfo = self.canEnterBasicInfo;
+        self.canEnterStatus = function () {
+            return userCreated && professionalCreated;
+        };
+    }
+
+    function prepareProfessionalModel(name, basicInfo, contactInfo) {
+        var model = {
+            name: name
+        };
+        model = angular.extend(model, basicInfo);
+        model.phone = contactInfo.phone;
+        model.address = angular.copy(contactInfo);
+        if (!model.address.postal_code) {
+            model.address.postal_code = '';
+        }
+        return model;
     }
 })();
