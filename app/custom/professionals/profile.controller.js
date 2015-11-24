4842d6351d0eef3d6e943364692b275059f262c9
Alexandr G <alexandr@gavrisco.com>
2015-11-24 18:33:35 +0200

    Add images to professional's profile controller.

--- app/custom/professionals/profile.controller.js
+++ app/custom/professionals/profile.controller.js
@@ -2,12 +2,24 @@
     'use strict';
 
     angular.module('bakimliProfessionals').controller('ProfessionalProfileController', [
-        'professional', professionalCtrl
+        '$state', '$stateParams', 'professional', 'ProfessionalPhotos', professionalCtrl
         ]);
 
-    function professionalCtrl(professional) {
+    function professionalCtrl($state, $stateParams, professional, professionalPhotos) {
         var self = this;
+        var setupPhotos = function () {
+            professionalPhotos.query({professional: professional.pk}, function (result) {
+                self.gallery.images = result.results;
+                self.gallery.total = result.count;
+            });
+        };
+        
+        self.gallery = {
+                images: []
+            };
 
         self.professional = professional;
+        self.activeTab = $stateParams.view || 'basic';
+        setupPhotos();
     }
 })();
