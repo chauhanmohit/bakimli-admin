4c5ee0fcbfc1de1590f587ce8e519e68da571cee
Mohit <mss.chauhanmohit@gmail.com>
2015-11-25 18:12:29 +0530

    professionallist

--- app/custom/professionals/professionals.routes.js
+++ app/custom/professionals/professionals.routes.js
@@ -59,6 +59,20 @@
                     }
                 }]
             }
+        }).state('restricted.professionals.list', {
+            url: '/list',
+            templateUrl: '/app/views/professionals/list.html',
+            controller: 'ProfessionalListController as professionalCtrl',
+            data: {
+                pageTitle: "Professional list",
+                redirectTo: 'restricted.professionals.list'
+                permissions: ['HAS_PROFILE', 'IS_APPROVED']
+            },
+            resolve: {
+                professional: ['$rootScope', 'Professionals', function ($rootScope, professionals) {
+                   return professionals.get();
+                }]
+            }
         });
     }
 })();
